662bf36323f60386663be9bdcbec3b0bc921a8ba
Dinis-Lei <dinislei@ua.pt>
2022-06-20 21:49:51 +0100

    TQSG11-86 implemented get product by id IT

--- src/test/java/tqs/g11/zap/integrationTests/RestControllerIT.java
+++ src/test/java/tqs/g11/zap/integrationTests/RestControllerIT.java
@@ -1,9 +1,6 @@
 package tqs.g11.zap.integrationTests;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -20,55 +17,30 @@ import org.springframework.http.ResponseEntity;
 //import org.springframework.test.context.TestPropertySource;
 
 import tqs.g11.zap.dto.AuthToken;
-import tqs.g11.zap.dto.CartCheckoutPostDTO;
-import tqs.g11.zap.dto.CartProductsRE;
+
 import tqs.g11.zap.dto.LoginRE;
 import tqs.g11.zap.dto.LoginUser;
 import tqs.g11.zap.dto.SignupRE;
 import tqs.g11.zap.dto.UserDto;
 import tqs.g11.zap.enums.UserRoles;
-import tqs.g11.zap.model.CartProduct;
+
 import tqs.g11.zap.model.Product;
 import tqs.g11.zap.model.User;
 import tqs.g11.zap.repository.CartRepository;
 import tqs.g11.zap.repository.ProductRepository;
 import tqs.g11.zap.repository.UsersRepository;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import org.springframework.http.MediaType;
-
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-import tqs.g11.zap.auth.TokenProvider;
-import tqs.g11.zap.auth.UnauthorizedEntryPoint;
 
-import tqs.g11.zap.enums.ErrorMsg;
 
-import tqs.g11.zap.service.CartService;
-import tqs.g11.zap.service.OrderService;
-import tqs.g11.zap.service.ProductService;
 import tqs.g11.zap.service.UsersService;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import org.aspectj.lang.annotation.Before;
+;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
@@ -93,38 +65,25 @@ class RestControllerIT {
     @Autowired
     private UsersService usersService;
 
-    User user1 = new User("user1", "Caio Costela", "amogus123", UserRoles.MANAGER);
-    User user2 = new User("user2", "Deinis Lie", "sussybot564", UserRoles.CLIENT);
-    User user3 = new User("user3", "Licius Vinicious", "edinaldus", UserRoles.CLIENT);
+    private User user1 = new User("user1", "Caio Costela", "amogus123", UserRoles.MANAGER);
+    private User user2 = new User("user2", "Deinis Lie", "sussybot564", UserRoles.CLIENT);
+    private User user3 = new User("user3", "Licius Vinicious", "edinaldus", UserRoles.CLIENT);
 
-    Product p1 = new Product(1L, "Among Us Pen Drive", "url1", "An Among Us pen drive", 69, user1, 420.69, "Pen Drive");
-    Product p2 = new Product(2L, "Notebook super charger", "url2", "A notebook charger", 40, user1, 69.0, "Charger");
-    Product p3 = new Product(3L, "Cellphone super charger", "url3", "An Among Us pen drive", 13, user1, 23.4, "Charger");
+    private Product p1 = new Product(1L, "Among Us Pen Drive", 
+                                    "url1", "An Among Us pen drive", 69, 
+                                    user1, 420.69, "Pen Drive"
+    );
+    private Product p2 = new Product(2L, "Notebook super charger", 
+                                    "url2", "A notebook charger", 40, 
+                                    user1, 69.0, "Charger"
+    );
+    private Product p3 = new Product(3L, "Cellphone super charger", 
+                                    "url3", "An Among Us pen drive", 13, 
+                                    user1, 23.4, "Charger"
+    );
 
+    private AuthToken authToken;
 
-
-    AuthToken authToken;
-
-    private ObjectMapper objectMapper = new ObjectMapper();
-
-
-    
-    void setUp() {
-        productRep.save(p1);
-        productRep.save(p2);
-        productRep.save(p3);
-        productRep.flush();
-
-        CartProduct cp1 = new CartProduct(1L, p1, 1, user2);
-        CartProduct cp2 = new CartProduct(2L, p3, 3, user2);
-
-        List<CartProduct> cpsUser2 = new ArrayList<>(Arrays.asList(cp1, cp2));
-    }
-
-    @AfterEach
-    void resetDb() {
-        
-    }
 
     @Test
     void runTests() throws JsonProcessingException{
@@ -132,6 +91,7 @@ class RestControllerIT {
         login();
         createProducts();
         getProducts();
+        getProductById();
     }
 
 
@@ -235,6 +195,21 @@ class RestControllerIT {
         assertThat(response.getBody())
             .extracting(Product::getProductId)
             .contains(p1.getProductId(),p2.getProductId(),p3.getProductId());
+    }
+
+
+    void getProductById(){
+        ResponseEntity<Product> response = restTemplate.exchange(
+            "/zap/products/1",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<Product>(){}
+        );
+
+        assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(response.getBody().getProductId())
+            .isEqualTo(p1.getProductId());
+
     }
 
     // @Test
